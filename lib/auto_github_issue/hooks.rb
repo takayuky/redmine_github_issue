443d8215a51cda30c66689a28eb68fcf0d29fc2b
Takayuki Sakai <kaky0922@gmail.com>
2015-03-05 20:23:12 +0900

    create a github issue right after making a Redmine ticket
    using basic auth in accessing github api

--- lib/auto_github_issue/hooks.rb
+++ lib/auto_github_issue/hooks.rb
@@ -2,8 +2,21 @@ module AutoGithubIssue
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_new_after_save(context={ })
       issue = context[:issue]
-      request = context[:request]
-      issue.description = "this issue's url is #{request.protocol}#{request.domain}/issues/#{issue.id}\n\n#{issue.description}"
+
+      uri = URI.parse("https://api.github.com")
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Post.new("/repos/sakai-takayuki/redmine-github-test/issues")
+      request.basic_auth('username', 'pass') # TODO this code shoule be replaced
+      request.body = {
+        title: issue.subject,
+        body: "This issue is associated with [this Redmine ticket](#{context[:request].protocol}#{context[:request].domain}/issues/#{issue.id})\n\n#{issue.description}."
+      }.to_json
+
+      response = https.request(request)
+
+      issue.description = "#{JSON.parse(response.body)['html_url']}\n\n#{issue.description}"
       issue.save
     end
   end
