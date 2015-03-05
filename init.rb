e3f0919f4ecc10f341742fc3deee76c2bd1d901c
Takayuki Sakai <kaky0922@gmail.com>
2015-03-05 16:42:07 +0900

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :auto_github_issue do
+  name 'Auto Github Issue Creator'
+  author 'Takayuki Sakai'
+  description 'Plugin which makes a github issue automatically on creating a new redmine ticket'
+  version '0.0.1'
+  url 'https://github.com/TakayukiSakai/auto_github_issue'
+  author_url 'https://github.com/TakayukiSakai'
+end
