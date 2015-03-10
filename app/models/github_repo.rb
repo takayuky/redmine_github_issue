daad5ec688026f3a5ef0210aea547fcbaa007adb
Takayuki Sakai <kaky0922@gmail.com>
2015-03-10 15:16:02 +0900

    add validation on github_repo model which checks the data is correct to access to the github repository

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -8,4 +8,20 @@ class GithubRepo < ActiveRecord::Base
   validates :project_id, presence: true
   validates :name, presence: true
   validates :owner, presence: true
+
+  validate :github_auth?
+
+  def github_auth?
+    uri = URI.parse("https://api.github.com")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new("/repos/#{owner}/#{name}")
+    request.basic_auth(user, pass)
+
+    response = https.request(request)
+    unless response.code == "200"
+      errors.add(:data, "seems to be wrong. We couldn't access to the github repository")
+    end
+  end
 end
