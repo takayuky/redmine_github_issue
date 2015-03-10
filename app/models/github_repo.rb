ac3feb8b0da6b117b81cb28bc14c60c25d5e6fc3
Takayuki Sakai <kaky0922@gmail.com>
2015-03-10 14:58:16 +0900

    add validation to github_repo model

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -1,7 +1,11 @@
 class GithubRepo < ActiveRecord::Base
   unloadable
-  attr_accessible :project_id, :name, :owner, :user, :pass
 
   belongs_to :project
   has_many :issues
+
+  attr_accessible :project_id, :name, :owner, :user, :pass
+  validates :project_id, presence: true
+  validates :name, presence: true
+  validates :owner, presence: true
 end
