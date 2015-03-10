c49514a82b71d3ac4bca6de7c775a86d6439ee7e
Takayuki Sakai <kaky0922@gmail.com>
2015-03-10 14:57:45 +0900

    post github api only when github repository is specified

--- lib/redmine_github_issue/hooks.rb
+++ lib/redmine_github_issue/hooks.rb
@@ -2,22 +2,25 @@ module AutoGithubIssue
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_new_after_save(context={})
       issue = context[:issue]
+      github_repo = issue.github_repo
 
-      uri = URI.parse("https://api.github.com")
-      https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = true
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Post.new("/repos/#{GITHUB_CONFIG['repository_owner']}/#{GITHUB_CONFIG['repository']}/issues")
-      request.basic_auth(GITHUB_CONFIG['username'], GITHUB_CONFIG['password'])
-      request.body = {
-        title: issue.subject,
-        body: "This issue is associated with [this Redmine ticket](#{context[:request].protocol}#{context[:request].domain}/issues/#{issue.id})\n\n#{issue.description}."
-      }.to_json
+      unless github_repo.nil?
+        uri = URI.parse("https://api.github.com")
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Post.new("/repos/#{github_repo.owner}/#{github_repo.name}/issues")
+        request.basic_auth(github_repo.user, github_repo.pass)
+        request.body = {
+          title: issue.subject,
+          body: "This issue is associated with [this Redmine ticket](#{context[:request].protocol}#{context[:request].domain}/issues/#{issue.id}).\n\n#{issue.description}"
+        }.to_json
 
-      response = https.request(request)
+        response = https.request(request)
 
-      issue.description = "#{JSON.parse(response.body)['html_url']}\n\n#{issue.description}"
-      issue.save
+        issue.description = "#{JSON.parse(response.body)['html_url']}\n\n#{issue.description}"
+        issue.save
+      end
     end
 
     def view_issues_show_description_bottom(context={})
